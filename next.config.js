5edf20160874141297407aac85b97301fb9d16da
Valentin Vila <vilavalentin@gmail.com>
2022-11-05 23:48:23 +0100

    initial build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
-}
+  exportPathMap: function() {
+    return {
+      '/': {page: '/'}
+    };
+  }
+};
 
 module.exports = nextConfig
